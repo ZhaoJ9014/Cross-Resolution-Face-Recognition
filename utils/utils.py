9b4cff22da4894a02fefbbce0ce829150f2e198c
= <=>
2019-04-20 17:36:49 +0800

    add acc calculation

--- utils/utils.py
+++ utils/utils.py
@@ -1,10 +1,91 @@
 import pandas as pd
 import csv
 import os
+import numpy as np
 import sys
 import torch
 import shutil
 import pickle
+from sklearn.model_selection import KFold
+from sklearn.decomposition import PCA
+import sklearn
+from scipy import interpolate
+
+def calculate_accuracy(threshold, dist, actual_issame):
+    predict_issame = np.less(dist, threshold)
+    tp = np.sum(np.logical_and(predict_issame, actual_issame))
+    fp = np.sum(np.logical_and(predict_issame, np.logical_not(actual_issame)))
+    tn = np.sum(np.logical_and(np.logical_not(predict_issame), np.logical_not(actual_issame)))
+    fn = np.sum(np.logical_and(np.logical_not(predict_issame), actual_issame))
+
+    tpr = 0 if (tp + fn == 0) else float(tp) / float(tp + fn)
+    fpr = 0 if (fp + tn == 0) else float(fp) / float(fp + tn)
+    acc = float(tp + tn) / dist.size
+    return tpr, fpr, acc
+
+def calculate_roc(thresholds, embeddings1, embeddings2, actual_issame, nrof_folds=50, pca=0):
+    assert (embeddings1.shape[0] == embeddings2.shape[0])
+    assert (embeddings1.shape[1] == embeddings2.shape[1])
+    nrof_pairs = min(len(actual_issame), embeddings1.shape[0])
+    nrof_thresholds = len(thresholds)
+    k_fold = KFold(n_splits=nrof_folds, shuffle=True)
+    
+    tprs = np.zeros((nrof_folds, nrof_thresholds))
+    fprs = np.zeros((nrof_folds, nrof_thresholds))
+    accuracy = np.zeros((nrof_folds))
+    best_thresholds = np.zeros((nrof_folds))
+    indices = np.arange(nrof_pairs)
+    # print('pca', pca)
+    
+    if pca == 0:
+        diff = np.subtract(embeddings1, embeddings2)
+        # pdb.set_trace()
+        dist = np.sum(np.square(diff), axis=1)
+        # dist = (dist)/(dist.max()-dist.min())
+        # dist = pdist(np.vstack([embeddings1, embeddings2]), 'cosine')
+        # dist.sort()
+        margin_list = [sorted(dist)[i + 1] - sorted(dist)[i] for i in range(np.shape(dist)[0] - 1)]
+        margin_max = max(margin_list)
+        margin_min = min(margin_list)
+    
+    for fold_idx, (train_set, test_set) in enumerate(k_fold.split(indices)):
+        # print('train_set', train_set)
+        # print('test_set', test_set)
+        if pca > 0:
+            print("doing pca on", fold_idx)
+            embed1_train = embeddings1[train_set]
+            embed2_train = embeddings2[train_set]
+            _embed_train = np.concatenate((embed1_train, embed2_train), axis=0)
+            # print(_embed_train.shape)
+            pca_model = PCA(n_components=pca)
+            pca_model.fit(_embed_train)
+            embed1 = pca_model.transform(embeddings1)
+            embed2 = pca_model.transform(embeddings2)
+            embed1 = sklearn.preprocessing.normalize(embed1)
+            embed2 = sklearn.preprocessing.normalize(embed2)
+            # print(embed1.shape, embed2.shape)
+            diff = np.subtract(embed1, embed2)
+            dist = np.sum(np.square(diff), 1)
+        
+        # Find the best threshold for the fold
+        acc_train = np.zeros((nrof_thresholds))
+        for threshold_idx, threshold in enumerate(thresholds):
+            _, _, acc_train[threshold_idx] = calculate_accuracy(threshold, dist[train_set], actual_issame[train_set])
+        best_threshold_index = np.argmax(acc_train)
+        #         print('best_threshold_index', best_threshold_index, acc_train[best_threshold_index])
+        best_thresholds[fold_idx] = thresholds[best_threshold_index]
+        for threshold_idx, threshold in enumerate(thresholds):
+            tprs[fold_idx, threshold_idx], fprs[fold_idx, threshold_idx], _ = calculate_accuracy(threshold,
+                                                                                                 dist[test_set],
+                                                                                                 actual_issame[
+                                                                                                     test_set])
+        _, _, accuracy[fold_idx] = calculate_accuracy(thresholds[best_threshold_index], dist[test_set],
+                                                      actual_issame[test_set])
+    
+    tpr = np.mean(tprs, 0)
+    fpr = np.mean(fprs, 0)
+    return tpr, fpr, accuracy.mean(), best_thresholds
+
 
 def load_state_dict(model, fname):
     """
